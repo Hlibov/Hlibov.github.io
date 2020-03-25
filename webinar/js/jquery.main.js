28856d78bca7880b33dddd674750628adbc06432
Hlibov <49684027+Hlibov@users.noreply.github.com>
2020-03-25 16:37:28 +0200

    Add files via upload

---
+++ webinar/js/jquery.main.js
@@ -0,0 +1,633 @@
+jQuery(function() {
+	initFormValidation();
+	initAnchors();
+});
+
+
+function initFormValidation() {
+	jQuery('.form-register').formValidation({
+		errorClass: 'error-holder',
+		addClassToParent: '.input-holder'
+	});
+}
+
+
+/*
+ * jQuery form validation plugin
+ */
+;(function($) {
+
+	'use strict';
+
+	var FormValidation = (function() {
+		var Validator = function($field, $fields) {
+			this.$field = $field;
+			this.$fields = $fields;
+		};
+
+		Validator.prototype = {
+			reg: {
+				email: '^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$',
+				number: '^[0-9]+$'
+			},
+
+			checkField: function() {
+				return {
+					state: this.run(),
+					$fields: this.$field.add(this.additionalFields)
+				}
+			},
+
+			run: function() {
+				var fieldType;
+
+				switch (this.$field.get(0).tagName.toUpperCase()) {
+					case 'SELECT':
+						fieldType = 'select';
+						break;
+
+					case 'TEXTAREA':
+						fieldType = 'text';
+						break;
+
+					default:
+						fieldType = this.$field.data('type') || this.$field.attr('type');
+				}
+
+				var functionName = 'check_' + fieldType.replace(/-/g,'_');
+				var state = true;
+
+				if ($.isFunction(this[functionName])) {
+					state = this[functionName]();
+
+					if (state && this.$field.data('confirm')) {
+						state = this.check_confirm();
+					}
+				}
+
+				return state;
+			},
+
+			check_email: function() {
+				var value = this.getValue();
+				var required = this.$field.data('required');
+				var requiredOrValue = required || value.length;
+
+				if ((requiredOrValue && !this.check_regexp(value, this.reg.email))) {
+					return false;
+				}
+
+				return requiredOrValue ? true : null;
+			},
+
+			check_number: function() {
+				var value = this.getValue();
+				var required = this.$field.data('required');
+				var isNumber = this.check_regexp(value, this.reg.number);
+				var requiredOrValue = required || value.length;
+
+				if (requiredOrValue && !isNumber) {
+					return false;
+				}
+
+				var min = this.$field.data('min');
+				var max = this.$field.data('max');
+				value = +value;
+
+				if ((min && (value < min || !isNumber)) || (max && (value > max || !isNumber))) {
+					return false;
+				}
+
+				return (requiredOrValue || min || max) ? true : null;
+			},
+
+			check_password: function() {
+				return this.check_text();
+			},
+
+			check_text: function() {
+				var value = this.getValue();
+				var required = this.$field.data('required');
+
+				if (this.$field.data('required') && !value.length) {
+					return false;
+				}
+
+				var min = +this.$field.data('min');
+				var max = +this.$field.data('max');
+
+				if ((min && value.length < min) || (max && value.length > max)) {
+					return false;
+				}
+
+				var regExp = this.$field.data('regexp');
+
+				if (regExp && !this.check_regexp(value, regExp)) {
+					return false;
+				}
+
+				return (required || min || max || regExp) ? true : null;
+			},
+
+			check_confirm: function() {
+				var value = this.getValue();
+				var $confirmFields = this.$fields.filter('[data-confirm="' + this.$field.data('confirm') + '"]');
+				var confirmState = true;
+
+				for (var i = $confirmFields.length - 1; i >= 0; i--) {
+					if ($confirmFields.eq(i).val() !== value || !value.length) {
+						confirmState = false;
+						break;
+					}
+				}
+
+				this.additionalFields = $confirmFields;
+
+				return confirmState;
+			},
+
+			check_select: function() {
+				var required = this.$field.data('required');
+
+				if (required && this.$field.get(0).selectedIndex === 0) {
+					return false;
+				}
+
+				return required ? true : null;
+			},
+
+			check_radio: function() {
+				var $fields = this.$fields.filter('[name="' + this.$field.attr('name') + '"]');
+				var required = this.$field.data('required');
+
+				if (required && !$fields.filter(':checked').length) {
+					return false;
+				}
+
+				this.additionalFields = $fields;
+
+				return required ? true : null;
+			},
+
+			check_checkbox: function() {
+				var required = this.$field.data('required');
+
+				if (required && !this.$field.prop('checked')) {
+					return false;
+				}
+
+				return required ? true : null;
+			},
+
+			check_at_least_one: function() {
+				var $fields = this.$fields.filter('[data-name="' + this.$field.data('name') + '"]');
+
+				if (!$fields.filter(':checked').length) {
+					return false;
+				}
+
+				this.additionalFields = $fields;
+
+				return true;
+			},
+
+			check_regexp: function(val, exp) {
+				return new RegExp(exp).test(val);
+			},
+
+			getValue: function() {
+				if (this.$field.data('trim')) {
+					this.$field.val($.trim(this.$field.val()));
+				}
+
+				return this.$field.val();
+			}
+		};
+
+		var publicClass = function(form, options) {
+			this.$form = $(form).attr('novalidate', 'novalidate');
+			this.options = options;
+		};
+
+		publicClass.prototype = {
+			buildSelector: function(input) {
+				return input + ':not(' + this.options.skipDefaultFields + (this.options.skipFields ? ',' + this.options.skipFields : '') + ')';
+			},
+
+			init: function() {
+				this.fieldsSelector = this.buildSelector(':input');
+
+				this.$form
+					.on('submit', this.submitHandler.bind(this))
+					.on('keyup blur', this.fieldsSelector, this.changeHandler.bind(this))
+					.on('change', this.buildSelector('select'), this.changeHandler.bind(this))
+					.on('focus', this.fieldsSelector, this.focusHandler.bind(this));
+			},
+
+			submitHandler: function(e) {
+				var self = this;
+				var $fields = this.getFormFields();
+
+				this.getClassTarget($fields)
+					.removeClass(this.options.errorClass + ' ' + this.options.successClass);
+
+				this.setFormState(true);
+
+				$fields.each(function(i, input) {
+					var $field = $(input);
+					var $classTarget = self.getClassTarget($field);
+
+					// continue iteration if $field has error class already
+					if ($classTarget.hasClass(self.options.errorClass)) {
+						return;
+					}
+
+					self.setState(new Validator($field, $fields).checkField());
+				});
+
+				return this.checkSuccess($fields, e);
+			},
+
+			checkSuccess: function($fields, e) {
+				var self = this;
+				var success = this.getClassTarget($fields || this.getFormFields())
+								  .filter('.' + this.options.errorClass).length === 0;
+
+				if (e && success && this.options.successSendClass) {
+					e.preventDefault();
+
+					$.ajax({
+						url: this.$form.removeClass(this.options.successSendClass).attr('action') || '/',
+						type: this.$form.attr('method') || 'POST',
+						data: this.$form.serialize(),
+						success: function() {
+							self.$form.addClass(self.options.successSendClass);
+						}
+					});
+				}
+
+				this.setFormState(success);
+
+				return success;
+			},
+
+			changeHandler: function(e) {
+				var $field = $(e.target);
+
+				if ($field.data('interactive')) {
+					this.setState(new Validator($field, this.getFormFields()).checkField());
+				}
+
+				this.checkSuccess();
+			},
+
+			focusHandler: function(e) {
+				var $field = $(e.target);
+
+				this.getClassTarget($field)
+					.removeClass(this.options.errorClass + ' ' + this.options.successClass);
+
+				this.checkSuccess();
+			},
+
+			setState: function(result) {
+				this.getClassTarget(result.$fields)
+					.toggleClass(this.options.errorClass, result.state !== null && !result.state)
+					.toggleClass(this.options.successClass, result.state !== null && this.options.successClass && !!result.state);
+			},
+
+			setFormState: function(state) {
+				if (this.options.errorFormClass) {
+					this.$form.toggleClass(this.options.errorFormClass, !state);
+				}
+			},
+
+			getClassTarget: function($input) {
+				return (this.options.addClassToParent ? $input.closest(this.options.addClassToParent) : $input);
+			},
+
+			getFormFields: function() {
+				return this.$form.find(this.fieldsSelector);
+			}
+		};
+
+		return publicClass;
+	}());
+
+	$.fn.formValidation = function(options) {
+		options = $.extend({}, {
+			errorClass: 'input-error',
+			successClass: '',
+			errorFormClass: '',
+			addClassToParent: '',
+			skipDefaultFields: ':button, :submit, :image, :hidden, :reset',
+			skipFields: '',
+			successSendClass: ''
+		}, options);
+
+		return this.each(function() {
+			new FormValidation(this, options).init();
+		});
+	};
+}(jQuery));
+
+
+// initialize smooth anchor links
+function initAnchors() {
+	new SmoothScroll({
+		anchorLinks: 'a[href^="#"]:not([href="#"])',
+		extraOffset: 0,
+		wheelBehavior: 'none'
+	});
+}
+
+
+/*!
+ * SmoothScroll module
+ */
+;(function($, exports) {
+	// private variables
+	var page,
+		win = $(window),
+		activeBlock, activeWheelHandler,
+		wheelEvents = ('onwheel' in document || document.documentMode >= 9 ? 'wheel' : 'mousewheel DOMMouseScroll');
+
+	// animation handlers
+	function scrollTo(offset, options, callback) {
+		// initialize variables
+		var scrollBlock;
+		if (document.body) {
+			if (typeof options === 'number') {
+				options = {
+					duration: options
+				};
+			} else {
+				options = options || {};
+			}
+			page = page || $('html, body');
+			scrollBlock = options.container || page;
+		} else {
+			return;
+		}
+
+		// treat single number as scrollTop
+		if (typeof offset === 'number') {
+			offset = {
+				top: offset
+			};
+		}
+
+		// handle mousewheel/trackpad while animation is active
+		if (activeBlock && activeWheelHandler) {
+			activeBlock.off(wheelEvents, activeWheelHandler);
+		}
+		if (options.wheelBehavior && options.wheelBehavior !== 'none') {
+			activeWheelHandler = function(e) {
+				if (options.wheelBehavior === 'stop') {
+					scrollBlock.off(wheelEvents, activeWheelHandler);
+					scrollBlock.stop();
+				} else if (options.wheelBehavior === 'ignore') {
+					e.preventDefault();
+				}
+			};
+			activeBlock = scrollBlock.on(wheelEvents, activeWheelHandler);
+		}
+
+		// start scrolling animation
+		scrollBlock.stop().animate({
+			scrollLeft: offset.left,
+			scrollTop: offset.top
+		}, options.duration, function() {
+			if (activeWheelHandler) {
+				scrollBlock.off(wheelEvents, activeWheelHandler);
+			}
+			if ($.isFunction(callback)) {
+				callback();
+			}
+		});
+	}
+
+	// smooth scroll contstructor
+	function SmoothScroll(options) {
+		this.options = $.extend({
+			anchorLinks: 'a[href^="#"]', // selector or jQuery object
+			container: null, // specify container for scrolling (default - whole page)
+			extraOffset: null, // function or fixed number
+			activeClasses: null, // null, "link", "parent"
+			easing: 'swing', // easing of scrolling
+			animMode: 'duration', // or "speed" mode
+			animDuration: 800, // total duration for scroll (any distance)
+			animSpeed: 1500, // pixels per second
+			anchorActiveClass: 'anchor-active',
+			sectionActiveClass: 'section-active',
+			wheelBehavior: 'stop', // "stop", "ignore" or "none"
+			useNativeAnchorScrolling: false // do not handle click in devices with native smooth scrolling
+		}, options);
+		this.init();
+	}
+	SmoothScroll.prototype = {
+		init: function() {
+			this.initStructure();
+			this.attachEvents();
+			this.isInit = true;
+		},
+		initStructure: function() {
+			var self = this;
+
+			this.container = this.options.container ? $(this.options.container) : $('html,body');
+			this.scrollContainer = this.options.container ? this.container : win;
+			this.anchorLinks = jQuery(this.options.anchorLinks).filter(function() {
+				return jQuery(self.getAnchorTarget(jQuery(this))).length;
+			});
+		},
+		getId: function(str) {
+			try {
+				return '#' + str.replace(/^.*?(#|$)/, '');
+			} catch (err) {
+				return null;
+			}
+		},
+		getAnchorTarget: function(link) {
+			// get target block from link href
+			var targetId = this.getId($(link).attr('href'));
+			return $(targetId.length > 1 ? targetId : 'html');
+		},
+		getTargetOffset: function(block) {
+			// get target offset
+			var blockOffset = block.offset().top;
+			if (this.options.container) {
+				blockOffset -= this.container.offset().top - this.container.prop('scrollTop');
+			}
+
+			// handle extra offset
+			if (typeof this.options.extraOffset === 'number') {
+				blockOffset -= this.options.extraOffset;
+			} else if (typeof this.options.extraOffset === 'function') {
+				blockOffset -= this.options.extraOffset(block);
+			}
+			return {
+				top: blockOffset
+			};
+		},
+		attachEvents: function() {
+			var self = this;
+
+			// handle active classes
+			if (this.options.activeClasses && this.anchorLinks.length) {
+				// cache structure
+				this.anchorData = [];
+
+				for (var i = 0; i < this.anchorLinks.length; i++) {
+					var link = jQuery(this.anchorLinks[i]),
+						targetBlock = self.getAnchorTarget(link),
+						anchorDataItem = null;
+
+					$.each(self.anchorData, function(index, item) {
+						if (item.block[0] === targetBlock[0]) {
+							anchorDataItem = item;
+						}
+					});
+
+					if (anchorDataItem) {
+						anchorDataItem.link = anchorDataItem.link.add(link);
+					} else {
+						self.anchorData.push({
+							link: link,
+							block: targetBlock
+						});
+					}
+				};
+
+				// add additional event handlers
+				this.resizeHandler = function() {
+					if (!self.isInit) return;
+					self.recalculateOffsets();
+				};
+				this.scrollHandler = function() {
+					self.refreshActiveClass();
+				};
+
+				this.recalculateOffsets();
+				this.scrollContainer.on('scroll', this.scrollHandler);
+				win.on('resize.SmoothScroll load.SmoothScroll orientationchange.SmoothScroll refreshAnchor.SmoothScroll', this.resizeHandler);
+			}
+
+			// handle click event
+			this.clickHandler = function(e) {
+				self.onClick(e);
+			};
+			if (!this.options.useNativeAnchorScrolling) {
+				this.anchorLinks.on('click', this.clickHandler);
+			}
+		},
+		recalculateOffsets: function() {
+			var self = this;
+			$.each(this.anchorData, function(index, data) {
+				data.offset = self.getTargetOffset(data.block);
+				data.height = data.block.outerHeight();
+			});
+			this.refreshActiveClass();
+		},
+		toggleActiveClass: function(anchor, block, state) {
+			anchor.toggleClass(this.options.anchorActiveClass, state);
+			block.toggleClass(this.options.sectionActiveClass, state);
+		},
+		refreshActiveClass: function() {
+			var self = this,
+				foundFlag = false,
+				containerHeight = this.container.prop('scrollHeight'),
+				viewPortHeight = this.scrollContainer.height(),
+				scrollTop = this.options.container ? this.container.prop('scrollTop') : win.scrollTop();
+
+			// user function instead of default handler
+			if (this.options.customScrollHandler) {
+				this.options.customScrollHandler.call(this, scrollTop, this.anchorData);
+				return;
+			}
+
+			// sort anchor data by offsets
+			this.anchorData.sort(function(a, b) {
+				return a.offset.top - b.offset.top;
+			});
+
+			// default active class handler
+			$.each(this.anchorData, function(index) {
+				var reverseIndex = self.anchorData.length - index - 1,
+					data = self.anchorData[reverseIndex],
+					anchorElement = (self.options.activeClasses === 'parent' ? data.link.parent() : data.link);
+
+				if (scrollTop >= containerHeight - viewPortHeight) {
+					// handle last section
+					if (reverseIndex === self.anchorData.length - 1) {
+						self.toggleActiveClass(anchorElement, data.block, true);
+					} else {
+						self.toggleActiveClass(anchorElement, data.block, false);
+					}
+				} else {
+					// handle other sections
+					if (!foundFlag && (scrollTop >= data.offset.top - 1 || reverseIndex === 0)) {
+						foundFlag = true;
+						self.toggleActiveClass(anchorElement, data.block, true);
+					} else {
+						self.toggleActiveClass(anchorElement, data.block, false);
+					}
+				}
+			});
+		},
+		calculateScrollDuration: function(offset) {
+			var distance;
+			if (this.options.animMode === 'speed') {
+				distance = Math.abs(this.scrollContainer.scrollTop() - offset.top);
+				return (distance / this.options.animSpeed) * 1000;
+			} else {
+				return this.options.animDuration;
+			}
+		},
+		onClick: function(e) {
+			var targetBlock = this.getAnchorTarget(e.currentTarget),
+				targetOffset = this.getTargetOffset(targetBlock);
+
+			e.preventDefault();
+			scrollTo(targetOffset, {
+				container: this.container,
+				wheelBehavior: this.options.wheelBehavior,
+				duration: this.calculateScrollDuration(targetOffset)
+			});
+			this.makeCallback('onBeforeScroll', e.currentTarget);
+		},
+		makeCallback: function(name) {
+			if (typeof this.options[name] === 'function') {
+				var args = Array.prototype.slice.call(arguments);
+				args.shift();
+				this.options[name].apply(this, args);
+			}
+		},
+		destroy: function() {
+			var self = this;
+
+			this.isInit = false;
+			if (this.options.activeClasses) {
+				win.off('resize.SmoothScroll load.SmoothScroll orientationchange.SmoothScroll refreshAnchor.SmoothScroll', this.resizeHandler);
+				this.scrollContainer.off('scroll', this.scrollHandler);
+				$.each(this.anchorData, function(index) {
+					var reverseIndex = self.anchorData.length - index - 1,
+						data = self.anchorData[reverseIndex],
+						anchorElement = (self.options.activeClasses === 'parent' ? data.link.parent() : data.link);
+
+					self.toggleActiveClass(anchorElement, data.block, false);
+				});
+			}
+			this.anchorLinks.off('click', this.clickHandler);
+		}
+	};
+
+	// public API
+	$.extend(SmoothScroll, {
+		scrollTo: function(blockOrOffset, durationOrOptions, callback) {
+			scrollTo(blockOrOffset, durationOrOptions, callback);
+		}
+	});
+
+	// export module
+	exports.SmoothScroll = SmoothScroll;
+}(jQuery, this));
